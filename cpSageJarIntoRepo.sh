72ed29623a41b5377677d4e78a4e596414beed3e
Mike Muldoon <mike@infrno.net>
2010-10-19 13:06:23 -0700

    Mommy's little helpers

---
+++ cpSageJarIntoRepo.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/bash
+
+SRC="/cygdrive/c/Program Files (x86)/Wowza Media Systems/Wowza Media Server 2.1.1/lib/sage.jar"
+DST=/cygdrive/c/Users/muldoon/Documents/workspace/sage/wowza/jar/
+cp -v "$SRC" $DST
+
