80e4d023509636d5975ad46ffe686e60b5cf55a8
Mike Muldoon <mike@infrno.net>
2011-01-30 23:17:56 -0800

    output formatting

--- admin/connectionSummary.sql
+++ admin/connectionSummary.sql
@@ -1,14 +1,3 @@
-select round(timestampdiff(second, connected_at, disconnected_at)/60/60, 1) as Hours,
-       room_name
-  from session_member 
- where connected_at <= date_sub(now(), interval 1 day) and
-       connected_at > date_sub(now(), interval 60 day) and
-       application_name = 'chat' and
-       room_name != "Sample Game" and 
-       user_id != 'muldoon'
-;
-
-
 select sum(round(timestampdiff(second, connected_at, disconnected_at)/60/60, 2)) as Hours
   from session_member 
  where connected_at <= date_sub(now(), interval 1 day) and
@@ -35,3 +24,13 @@ select count(distinct(user_name)) as 'Unique users'
 
 
 
+
+select round(timestampdiff(second, connected_at, disconnected_at)/60/60, 1) as Hours,
+       room_name
+  from session_member 
+ where connected_at <= date_sub(now(), interval 1 day) and
+       connected_at > date_sub(now(), interval 60 day) and
+       application_name = 'chat' and
+       room_name != "Sample Game" and 
+       user_id != 'muldoon'
+;
