d01ab6ada6d1fd34a99075825f4e4c2e7f27607e
Mike Muldoon <d00n2@hotmail.com>
2011-03-30 13:20:36 -0700

    removed primary from session_id def, cuz it aint

--- db/create_image_table.sql
+++ db/create_image_table.sql
@@ -1,7 +1,7 @@
 
 DROP TABLE IF EXISTS `image`;
 CREATE TABLE `image` (
-  `session_id` int(11) PRIMARY KEY DEFAULT 1,
+  `session_id` int(11) KEY DEFAULT 1,
   `created_on` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `path` varchar(255),
   KEY `image_created_on_idx` (`created_on`)
