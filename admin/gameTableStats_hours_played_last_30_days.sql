9e1e48fdd34e06d165a0ead5dec355f58838ee39
Mike Muldoon <d00n2@hotmail.com>
2011-12-18 22:03:38 -0800

    last 30 days

---
+++ admin/gameTableStats_hours_played_last_30_days.sql
@@ -0,0 +1,10 @@
+select sum(round(timestampdiff(second, connected_at, disconnected_at)/60/60, 2)) as 'Hours',
+       date_sub(now(), interval 32 day) as 'starting',
+       date_sub(now(), interval 1 day) as 'ending'
+  from session_member
+ where connected_at <= date_sub(now(), interval 1 day) and
+       connected_at > date_sub(now(), interval 32 day) and
+       disconnected_at !=0 and
+       application_name = 'whiteboard'
+;
+
