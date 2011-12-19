5ee7313d10b24d5293712d1090eee1d9c35a6699
Mike Muldoon <mike@infrno.net>
2011-12-18 22:13:54 -0800

    add hours/day to 30 day report

--- admin/gameTableStats_hours_played_last_30_days.sql
+++ admin/gameTableStats_hours_played_last_30_days.sql
@@ -1,6 +1,16 @@
 select sum(round(timestampdiff(second, connected_at, disconnected_at)/60/60, 2)) as 'Hours',
-       date_sub(now(), interval 32 day) as 'starting',
+       date_sub(now(), interval 31 day) as 'starting',
        date_sub(now(), interval 1 day) as 'ending'
+  from session_member
+ where connected_at <= date_sub(now(), interval 1 day) and
+       connected_at > date_sub(now(), interval 32 day) and
+       disconnected_at !=0 and
+       application_name = 'whiteboard'
+;
+
+select sum(round(timestampdiff(second, connected_at, disconnected_at)/60/60/30, 2)) as 'Hours/day',
+       date_sub(now(), interval 31 day),
+       date_sub(now(), interval 1 day)
   from session_member
  where connected_at <= date_sub(now(), interval 1 day) and
        connected_at > date_sub(now(), interval 32 day) and
