e08828d58009b9e3c5c68e6af1cd369f58865c78
root <root@ip-172-31-29-247.us-west-2.compute.internal>
2015-08-27 22:05:13 +0000

    tweaks for aws

--- db/create_user.sql
+++ db/create_user.sql
@@ -1,4 +1,4 @@
-create database sage;
+#create database sage;
 create user 'sage_rw'@'%' identified by 'sk00bysnack99';
 create user 'sage_rw'@'localhost' identified by 'sk00bysnack99';
 grant all on sage.* to 'sage_rw'@'%';
