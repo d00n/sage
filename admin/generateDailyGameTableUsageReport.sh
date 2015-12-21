bde09fe1062a087e814abb105b108fd6627730c9
EC2 Default User <ec2-user@ip-172-31-29-247.us-west-2.compute.internal>
2015-12-21 04:42:12 +0000

    RDS connection strings, refresh for latestURL, reorder daily report

--- admin/generateDailyGameTableUsageReport.sh
+++ admin/generateDailyGameTableUsageReport.sh
@@ -13,12 +13,6 @@ echo
 echo
 /usr/bin/mysql sage -u sage_rw -psk00bysnack99 --host=sage.c5p5kzhtwqwh.us-west-2.rds.amazonaws.com < $ADMIN_DIR/gameTableStats_hours_per_day_last_30_days.sql 
 echo
-echo 'Games in progress'
-/usr/bin/mysql sage -u sage_rw -psk00bysnack99 --host=sage.c5p5kzhtwqwh.us-west-2.rds.amazonaws.com < $ADMIN_DIR/gameTableStats_games_in_progress.sql
-echo
-echo 'Zombie connections'
-/usr/bin/mysql sage -u sage_rw -psk00bysnack99 --host=sage.c5p5kzhtwqwh.us-west-2.rds.amazonaws.com < $ADMIN_DIR/gameTableStats_zombie_connections.sql
-echo
 /usr/bin/mysql sage -u sage_rw -psk00bysnack99 --host=sage.c5p5kzhtwqwh.us-west-2.rds.amazonaws.com < $ADMIN_DIR/gameTableStats_unique_users.sql
 echo
 /usr/bin/mysql sage -u sage_rw -psk00bysnack99 --host=sage.c5p5kzhtwqwh.us-west-2.rds.amazonaws.com < $ADMIN_DIR/gameTableStats_hours_by_game.sql
@@ -30,3 +24,9 @@ echo "Top users by hours played"
 echo
 echo "Top games by hours played"
 /usr/bin/mysql sage -u sage_rw -psk00bysnack99 --host=sage.c5p5kzhtwqwh.us-west-2.rds.amazonaws.com < $ADMIN_DIR/gameTableStats_top_games_by_hours_played.sql
+echo
+echo 'Games in progress'
+/usr/bin/mysql sage -u sage_rw -psk00bysnack99 --host=sage.c5p5kzhtwqwh.us-west-2.rds.amazonaws.com < $ADMIN_DIR/gameTableStats_games_in_progress.sql
+echo
+echo 'Zombie connections'
+/usr/bin/mysql sage -u sage_rw -psk00bysnack99 --host=sage.c5p5kzhtwqwh.us-west-2.rds.amazonaws.com < $ADMIN_DIR/gameTableStats_zombie_connections.sql
--- httpd/html/services/index.py
+++ httpd/html/services/index.py
@@ -2,7 +2,7 @@ import MySQLdb
 
 # TODO: run this under https, and inspect credentials passed via POST
 def newRoomId(req):
-  conn = MySQLdb.connect(host='localhost',user='sage_rw',passwd='sk00bysnack99',db='sage')
+  conn = MySQLdb.connect(host='sage.c5p5kzhtwqwh.us-west-2.rds.amazonaws.com',user='root',passwd='sk00bysnack',db='sage')
   cursor = conn.cursor()
   cursor.execute('insert into room (created_on) values (now())')
   room_id = conn.insert_id()
@@ -10,7 +10,7 @@ def newRoomId(req):
   return room_id
 
 def getLatestImageURL(req):
-  conn = MySQLdb.connect(host='localhost',user='sage_rw',passwd='sk00bysnack99',db='sage')
+  conn = MySQLdb.connect(host='sage.c5p5kzhtwqwh.us-west-2.rds.amazonaws.com',user='root',passwd='sk00bysnack',db='sage')
   cursor = conn.cursor()
   cursor.execute('select path from image order by created_on desc limit 1')
   row = cursor.fetchone()
@@ -18,4 +18,4 @@ def getLatestImageURL(req):
   conn.close()
 
   url = "http://wowza.infrno.net%s" % row[0]
-  return url
+  return '<html><head><meta http-equiv="refresh" content="60; URL=http://wowza.infrno.net/services/getLatestImageURL"></head><img width="100%%" src="http://wowza.infrno.net%s"></html>' % row[0]
